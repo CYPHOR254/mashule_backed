17ccc70fddb254d124067ec13ff99f1a57fcbb4c
CYPHOR 254 <muneneearvine254@gmail.com>
2023-09-24 09:30:04 +0300

    2

---
+++ database.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const index = express()
+const mysql = require('mysql2/promise');
+
+
+
+const connection = mysql.createPool({
+  connectionLimit: 100,
+  host: "127.0.0.1",       //This is your localhost IP
+  user: "root",         // "newuser" created in Step 1(e)
+  password: "!asapmysql+2enen#",  // password for the new user
+  database: "mashuledb",      // Database name
+  port: "3306",          // port name, "3306" by default
+});
+
+connection.getConnection( (err, connection)=> {
+  if (err) throw (err)
+  console.log ("DB connected successful: " + connection.threadId)
+});
+
+module.exports = connection;
